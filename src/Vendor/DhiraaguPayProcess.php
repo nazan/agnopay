95c787c266e8bdde4316963bc629223c3de7f066
Hussain Nazan Naeem <nazan@allied.mv>
2021-06-24 09:49:54 +0500

    wip

--- src/Vendor/DhiraaguPayProcess.php
+++ src/Vendor/DhiraaguPayProcess.php
@@ -208,7 +208,7 @@ class DhiraaguPayProcess extends BaseVendorProcess
 				throw new ExternalSystemErrorException('Payload in response from Dhiraagu Pay has unexpected format. JSON data expected.');
 			}
 
-			return json_decode($response->getBody()->getContents(), true);
+			return $response->json();
         } catch(GuzzleException $excp) {
 			\Log::debug('guzzle call error.');
 			\Log::debug($excp->getResponse()->getBody()->getContents());
